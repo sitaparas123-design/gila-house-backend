5e848f9a49d68c853770ee2c3c11cce8fb0189e2
sitaparas123-design <sitaparas123@gmail.com>
2026-08-10 18:57:20 +0530

    Update socket.manager.js
    ghjjf

--- src/sockets/socket.manager.js
+++ src/sockets/socket.manager.js
@@ -10,6 +10,7 @@ const initSocket = (server) => {
         'http://localhost:3000',
         'https://restorentposback.softwaredemolive.live',
         'http://127.0.0.1:5173',
+        'https://app.gilahouse.com,
         process.env.FRONTEND_URL
       ].filter(Boolean),
       methods: ['GET', 'POST'],
